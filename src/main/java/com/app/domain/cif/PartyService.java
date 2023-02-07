59bb9f8ac0bc5d3378b59b4dc562c2adba279f3a
sreejithu panicker <sree.panicker@outlook.com>
2023-02-06 19:00:48 -0500

    Merge usecase with test

---
+++ src/main/java/com/app/domain/cif/PartyService.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.app.domain.cif;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author sreep
+ */
+@Service
+public class PartyService {
+    public PartyEntity merge (PartyEntity primary, PartyEntity secondary){
+        PartyEntity partyEnity = null;
+        //TODO combine two parties;
+        return partyEnity ;
+    }
+}
---
+++ src/test/java/com/app/domain/PartyServiceTests.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.app.domain;
+
+import com.app.domain.cif.PartyEntity;
+import com.app.domain.cif.PartyService;
+import com.app.domain.cif.Type;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ *
+ * @author sreep
+ */
+@ExtendWith(MockitoExtension.class)
+public class PartyServiceTests {
+
+    private PartyEntity partyEntityPri;
+    private PartyEntity partyEntitySec;
+    
+    
+    @InjectMocks
+    private PartyService partyService;
+
+    @BeforeEach
+    public void init() {
+        partyEntityPri = new PartyEntity("20", Type.SINGLE, "Rd 360 address");
+        partyEntitySec = new PartyEntity("21", Type.SINGLE, "Rd 400 address");
+    }
+
+    @Test
+    public void merge_party_success1() {
+        
+        
+       PartyEntity partyEntity = partyService.merge(partyEntityPri, partyEntityPri);
+       Assertions.assertEquals(null,partyEntity);
+
+    }
+
+}
